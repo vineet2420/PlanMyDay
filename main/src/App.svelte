46760a987bd7180b363209d4290f41552b81d7cf
Vineet Malhotra <vineet.malhotra125@gmail.com>
2021-12-03 23:46:33 -0500

    Implement SPA design for routing

--- main/src/App.svelte
+++ main/src/App.svelte
@@ -1,21 +1,13 @@
 <script>
+	import Router from "svelte-spa-router";
+
+	import Root from "./LandingPage.svelte";
+	import CalendarViewer from "./ViewPlanner.svelte";
+
+	let routes = {
+		"/": Root,
+		"/Plan": CalendarViewer,
+	};
 </script>
 
-<main>
-	<div id="tes">
-		<h1>Let's Get Started!</h1>
-		<button>Plan My Day</button>
-	</div>
-</main>
-
-<style>
-	#tes {
-		margin: 0;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		margin-right: -50%;
-		transform: translate(-50%, -50%);
-		text-align: center;
-	}
-</style>
+<Router { routes } />
---
+++ main/src/ViewPlanner.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { push, replace, location } from "svelte-spa-router";
+</script>
+
+<main>
+	<div id="tes">
+		<h1>Calendar Viewer</h1>
+		<button on:click={() => push("/")}>Go Back</button>
+	</div>
+</main>
+
+<style>
+	#tes {
+		margin: 0;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		margin-right: -50%;
+		transform: translate(-50%, -50%);
+		text-align: center;
+	}
+</style>
